795501993aaeb2b8f3eab098299f2fa32fb4210a
lauranevado <laura.nevado@edu.uah.es>
2024-05-25 00:35:04 +0200

    24 mayo LN

--- src/main/java/com/example/pruebafinal_/intento1/TableroControllers.java
+++ src/main/java/com/example/pruebafinal_/intento1/TableroControllers.java
@@ -764,7 +764,7 @@ public class TableroControllers implements Initializable {
             System.out.println("Partida cargada: (num turnos)" + datosCargadosPartida1.getTurnosJuego());
         }
     }
-    //////////////////////////////////////Bucle de control////////////////////////////////////////////
+    /////////////////////////////////////Bucle de control////////////////////////////////////////////
 
     @FXML
     public void updateTablero() {
